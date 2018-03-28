0aaa75d37733a3721345a1533673c78fd777827e
Felix Ödmark <c14fok@cs.umu.se>
2018-03-28 16:22:43 +0200

    Changed some syntax

--- ExampleClient.java
+++ ExampleClient.java
@@ -5,30 +5,25 @@ import java.net.SocketTimeoutException;
 import ki.types.ds.Block;
 import ki.types.ds.StreamInfo;
 
-public final class ExampleClient
-{
+public final class ExampleClient {
   public static final int DEFAULT_TIMEOUT = 1000;
 
 
   //----------------------------------------------------------
   //----------------------------------------------------------
   public static void listStreamInfo (StreamServiceClient client)
-    throws IOException
-  {
+    throws IOException {
     StreamInfo[] streams = client.listStreams();
     System.out.println("found " + streams.length + " streams");
-    for (StreamInfo stream : streams)
-    {
+    for (StreamInfo stream : streams) {
       System.out.println("  '" + stream.getName() + "': " + stream.getLengthInFrames() + " frames, " +
                          stream.getWidthInBlocks() + " x " + stream.getHeightInBlocks() + " blocks");
     }
   }
 
   //----------------------------------------------------------
-	public static void main (String[] args)
-	{
-    try
-    {
+	public static void main (String[] args) {
+    try {
       final String host     = (args.length > 0) ? args[0] : "localhost";
       final int timeout     = (args.length > 1) ? Integer.parseInt(args[1]) : DEFAULT_TIMEOUT;
       final String username = (args.length > 2) ? args[2] : "test";
@@ -43,25 +38,20 @@ public final class ExampleClient
       int frame = 0;
       int blockX = 0;
       int blockY = 0;
-      for (int i=0; i<nr; i++)
-      {
-        try
-        {
+      for (int i=0; i<nr; i++) {
+        try {
           long t1 = System.currentTimeMillis();
           Block block = client.getBlock(stream,frame,blockX,blockY);
           long t2 = System.currentTimeMillis();
           System.out.println("block received from server in " + (t2 - t1) + " ms");
           count++;
         }
-        catch (SocketTimeoutException e)
-        {
+        catch (SocketTimeoutException e) {
           System.out.println("socket timeout");
         }
       }
       System.out.println("received " + count + " / " + nr);
-	  }
-	  catch (Exception e)
-	  {
+    } catch (Exception e) {
 	    e.printStackTrace();
 	  }
 	}
